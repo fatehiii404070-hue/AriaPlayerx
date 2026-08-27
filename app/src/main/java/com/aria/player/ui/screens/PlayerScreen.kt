5a46ea376b4b8e2150ec9a22a506796a153ad322
fatehiii404070-hue <fatehiii404070@gmail.com>
2026-08-28 00:56:17 +0330

    Create PlayerScreen.kt

---
+++ app/src/main/java/com/aria/player/ui/screens/PlayerScreen.kt
@@ -0,0 +1,26 @@
+package com.aria.player.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun PlayerScreen() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(
+            text = "Player Screen"
+        )
+    }
+}
