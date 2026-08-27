4db4e760004d4ce614f0fd5bfbd3a64f6fe1fb66
fatehiii404070-hue <fatehiii404070@gmail.com>
2026-08-27 23:01:56 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -36,7 +37,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
 
     implementation("androidx.activity:activity-compose:1.9.1")
@@ -46,10 +46,13 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
 
     implementation("androidx.compose.ui:ui")
+
     implementation("androidx.compose.ui:ui-graphics")
+
     implementation("androidx.compose.ui:ui-tooling-preview")
 
     implementation("androidx.compose.material3:material3")
+
     implementation("androidx.compose.material:material-icons-extended")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
