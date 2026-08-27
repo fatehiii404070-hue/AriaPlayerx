4a583a397cc05318829a0703536a74ba91c1ca27
fatehiii404070-hue <fatehiii404070@gmail.com>
2026-08-28 02:24:01 +0330

    Update MainActivity.kt

--- app/src/main/java/com/aria/player/MainActivity.kt
+++ app/src/main/java/com/aria/player/MainActivity.kt
@@ -3,16 +3,20 @@ package com.aria.player
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.aria.player.ui.screens.HomeScreen
+import com.aria.player.ui.screens.VideoFeedScreen
+
 
 class MainActivity : ComponentActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
 
         setContent {
 
-            HomeScreen()
+            VideoFeedScreen()
 
         }
     }
